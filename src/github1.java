532c6b90b7f177f5a554d53f533b0e60267ee824
Moyna Begum <moynabegum@Moynas-MacBook-Pro.local>
2018-04-07 15:12:52 -0400

    whatever

--- src/github1.java
+++ src/github1.java
@@ -2,5 +2,6 @@ public class github1 {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("hello");
+        System.out.println("myname is moyna");
     }
 }
